b3146b18d3a42f06b60bee708f9e5d90d927793f
Trisha Meyer <tmeye62@gmail.com>
2016-02-10 20:49:56 -0800

    trying to fix ability to create board when you click a button

--- trisha.js
+++ trisha.js
@@ -1,21 +1,27 @@
-//GLOBAL VARIABLES//
-var player_moves_moves_array = ['','','','','','','','',''];
+var current_square = null;
+var p1;
+var p2;
+var player = null;
+var turn_counter = 0;
+var id_number = 0;
+var indicator_number = 1;
 
+$(document).ready(function(){
 
+//@purpose:
+//@params:
+//@return:
+//@globals:
+    $('#three_by').click(function(){
+        create3x3();
+    });
 
 
 
 //@purpose: hold the click handler for each 'square' on our game board and allows them all to be clicked and then call to other functions
 //@params: none
 //@return: none
-//@global:
-var current_square = null;
-var p1;
-var p2;
-var player = null;
-var turn_counter = 0;
-
-$(document).ready(function(){
+//@global: none
     $('.square').click(function(){
         console.log('square clicked');
         current_square = $(this);
@@ -25,6 +31,11 @@ $(document).ready(function(){
         $(this).attr('disabled', true);
     });
 
+
+//@purpose:
+//@params:
+//@return:
+//@globals:
     //click on id='X' and it sets player1 to the (this).text('X')
      $('.player_sign').click(function(){
        console.log('choosing player button clicked');
@@ -33,6 +44,11 @@ $(document).ready(function(){
      });
 });
 
+
+//@purpose:
+//@params:
+//@return:
+//@globals:
 function store_square(square_value){
     console.log('store square called');
     moves_array[square_value] += player;
@@ -40,6 +56,11 @@ function store_square(square_value){
     current_square.text(player);
 }
 
+
+//@purpose:
+//@params:
+//@return:
+//@globals:
  function player_select(button_value){
    if (button_value === 'X'){
      p1 = button_value;
@@ -54,6 +75,11 @@ function store_square(square_value){
  }
 
 
+
+//@purpose:
+//@params:
+//@return:
+//@globals:
  function player_turn(){
    console.log('selecting player function');
    if(turn_counter %2 === 0){
@@ -64,3 +90,27 @@ function store_square(square_value){
       player = p2;
    }
  }
+
+
+//@purpose:
+//@params:
+//@return:
+//@globals:
+function create3x3(){
+    var new_button_container = $('<div>').addClass('col-xs-12 button_container');
+    var new_parent_row = $('<div>').addClass('row');
+    for (var i=0; i<3; i++){
+        var new_row = $('<div>').addClass('row square_row');
+        for(var j=0; j<3; j++){
+            var new_button = $('<button>').addClass('square col-xs-4').attr({id:id_number, indicator:indicator_number});
+            id_number++;
+            indicator_number+= indicator_number;
+            console.log(indicator_number);
+            $(new_row).append(new_button);
+        }
+        $(new_button_container).append(new_row);
+    }
+    $(new_parent_row).append(new_button_container);
+    $('#game_area').append(new_parent_row);
+
+}
